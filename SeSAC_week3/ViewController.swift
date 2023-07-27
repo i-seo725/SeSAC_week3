09e47923583d93af0ad1ee068597977b64e32d8d
i-seo725 <hjing719@gmail.com>
2023-07-27 10:29:29 +0900

    Initial Commit

---
+++ SeSAC_week3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_week3
+//
+//  Created by 이은서 on 2023/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
