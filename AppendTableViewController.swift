06b306124c60c6d821764dff9d46d05cfdc9b8bd
i-seo725 <hjing719@gmail.com>
2023-07-27 21:25:09 +0900

    과제 완료

---
+++ AppendTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AppendTableViewController.swift
+//  SeSAC_week3
+//
+//  Created by 이은서 on 2023/07/27.
+//
+
+import UIKit
+
+class AppendTableViewController: UITableViewController {
+
+    @IBOutlet var keywordTextField: UITextField!
+    @IBOutlet var addButton: UIButton!
+    
+    var todoList = ["그립톡 구매하기", "사이다 구매", "아이패드 케이스 최저가 알아보기", "양말"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        designSetting()
+    }
+    func designSetting() {
+        keywordTextField.placeholder = "무엇을 구매하실 건가요?"
+        keywordTextField.backgroundColor = .systemGray6
+        keywordTextField.layer.cornerRadius = 15
+        addButton.setTitle("추가", for: .normal)
+        addButton.tintColor = .black
+        addButton.layer.backgroundColor = UIColor.systemGray5.cgColor
+        addButton.layer.cornerRadius = 8
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell")!
+        cell.textLabel?.text = todoList[indexPath.row]
+        
+        cell.imageView?.image = indexPath.row != 0 ? UIImage(systemName: "checkmark.square") : UIImage(systemName: "checkmark.square.fill")
+        return cell
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: Any) {
+        guard let newKeyword = keywordTextField.text else {
+            return
+        }
+        todoList.append(newKeyword)
+        tableView.reloadData()
+    }
+    
+}
---
+++ SeSAC_week3/Assignment/SettingTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SettingTableViewController.swift
+//  SeSAC_week3
+//
+//  Created by 이은서 on 2023/07/27.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    let sectionTitle = ["전체 설정", "개인 설정", "기타"]
+    let cellTitle = [["공지사항", "실험실", "버전 정보"], ["개인/보안", "알림", "채팅", "멀티프로필"], ["고객센터/도움말"]]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitle.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        for i in 0...sectionTitle.count - 1 {
+            if section == i {
+                return sectionTitle[i]
+            } else { print("섹션 제목 생성 시 오류 발생")}
+        }
+        return "오류 발생"
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        for i in 0...cellTitle.count - 1 {
+            if section == i {
+                return cellTitle[i].count
+            } else { print("셀 생성 시 오류 발생") }
+        }
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell")!
+        for i in 0...sectionTitle.count - 1 {
+            if indexPath.section == i {
+                cell.textLabel?.text = cellTitle[i][indexPath.row]
+                return cell
+            } else { print("셀 제목 생성 시 오류 발생")}
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
