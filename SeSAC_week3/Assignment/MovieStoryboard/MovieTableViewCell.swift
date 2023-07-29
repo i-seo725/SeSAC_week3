a885b78870fbb9a038f6a38415579d141271eb81
i-seo725 <hjing719@gmail.com>
2023-07-29 09:51:23 +0900

    화면 레이아웃 잡기

---
+++ SeSAC_week3/Assignment/MovieStoryboard/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  SeSAC_week3
+//
+//  Created by 이은서 on 2023/07/29.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
