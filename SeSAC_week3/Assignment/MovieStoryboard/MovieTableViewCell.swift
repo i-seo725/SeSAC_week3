bfa392e678ecd0554d3d772e59160e9cf55a00a4
i-seo725 <hjing719@gmail.com>
2023-07-29 09:55:03 +0900

    뷰 객체 연결

--- SeSAC_week3/Assignment/MovieStoryboard/MovieTableViewCell.swift
+++ SeSAC_week3/Assignment/MovieStoryboard/MovieTableViewCell.swift
@@ -9,15 +9,10 @@ import UIKit
 
 class MovieTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet var moviePosterImageView: UIImageView!
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var movieSubTitleLabel: UILabel!
+    @IBOutlet var movieSummaryTextView: UITextView!
+    
 
 }
