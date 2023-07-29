bd8a027caa7bb812cfb1bafe6f14c93e27dcd2e8
i-seo725 <hjing719@gmail.com>
2023-07-29 10:25:21 +0900

    과제 완료

---
+++ SeSAC_week3/Assignment/MovieStoryboard/MovieStruct.swift
@@ -0,0 +1,16 @@
+//
+//  MovieStruct.swift
+//  SeSAC_week3
+//
+//  Created by 이은서 on 2023/07/29.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    let releaseDate: String
+    let runtime: Int
+    let overview: String
+    let rate: Double
+}
--- SeSAC_week3/Assignment/MovieStoryboard/MovieTableViewController.swift
+++ SeSAC_week3/Assignment/MovieStoryboard/MovieTableViewController.swift
@@ -8,82 +8,31 @@
 import UIKit
 
 class MovieTableViewController: UITableViewController {
-
+    
+    let movieInformation = MovieInfo()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.rowHeight = 150
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
+    //셀 개수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        movieInformation.movie.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell") as! MovieTableViewCell
+        
+        let getMovieInfo = movieInformation.movie[indexPath.row]
+        cell.moviePosterImageView.image = UIImage(named: "\(getMovieInfo.title)")
+        cell.movieTitleLabel.text = getMovieInfo.title
+        cell.movieTitleLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        cell.movieSubTitleLabel.text = "\(getMovieInfo.releaseDate) | \(getMovieInfo.runtime)분 | \(getMovieInfo.rate)점"
+        cell.movieSubTitleLabel.font = UIFont.systemFont(ofSize: 14)
+        cell.movieSummaryTextView.text = "\(getMovieInfo.overview)"
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
